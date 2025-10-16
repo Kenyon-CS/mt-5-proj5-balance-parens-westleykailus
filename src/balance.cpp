4052e519457a4dd4915c4560873c5126c0279e59
westleykailus <kailus1@kenyon.edu>
2025-10-16 14:22:04 -0400

    completed is_balanced_rec

--- src/balance.cpp
+++ src/balance.cpp
@@ -4,9 +4,17 @@
 static bool is_balanced_rec(const std::string& s, int idx, int open) {
     // TODO: implement recursively.
     // - If open < 0 => false
+    if (open < 0) return false;
     // - If idx == s.size() => open == 0
+    else if (idx == s.size()) open = 0;
     // - If '(' => recurse open+1
+    else if (s == "(") {
+        is_balanced_rec(s, idx, open + 1);
+    }
     // - If ')' => recurse open-1
+    else if (s == ")"){
+        is_balanced_rec(s, idx, open - 1);
+    }
     // - else ignore
     return false;
 }
